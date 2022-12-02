f5cdc5cb54abb5b50862557ad678030d3a6818ca
X7riend <shakiya.friend0107@gmail.com>
2022-12-01 20:36:37 -0500

    InvoiceViewModel Validations

--- src/main/java/com/company/FinalProject/viewModel/InvoiceViewModel.java
+++ src/main/java/com/company/FinalProject/viewModel/InvoiceViewModel.java
@@ -1,20 +1,48 @@
 package com.company.FinalProject.viewModel;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import java.math.BigDecimal;
 import java.util.Objects;
 
 public class InvoiceViewModel {
 
+    @NotEmpty(message = "Must have id.")
     private int id;
+
+    @NotEmpty(message = "Must enter a name.")
     private String name;
+
+    @NotEmpty(message = "Must enter a street.")
     private String street;
+
+    @NotEmpty(message = "Must enter a city.")
     private String city;
+
+    @NotEmpty(message = "Must enter a state.")
+    @Length(max = 2, message = "Must enter state's abbreviation.")
     private String state;
+
+    @NotEmpty(message = "Must enter a zipcode.")
+    @Min(value = 5, message = "Zipcode must be 5 digits")
+    @Max(value = 5, message = "Zipcode must be 5 digits")
     private String zipcode;
+
+    @NotEmpty(message = "Must specify item type.")
     private String item_type;
+
+    @NotEmpty(message = "Must specify item id.")
     private int item_id;
     private BigDecimal unit_price;
+
+    @NotEmpty(message = "Must specific item quantity.")
+    @Min(value = 1, message = "Item quantity must be greater than 0.")
     private int quantity;
+
     private BigDecimal subtotal;
     private BigDecimal tax;
     private BigDecimal processing_fee;
