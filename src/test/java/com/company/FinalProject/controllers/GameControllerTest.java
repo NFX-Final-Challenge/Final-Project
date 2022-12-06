86d0445eaa725a3d3b05657ff62ca82368141d96
oem123 <acuzeau@stedwards.edu>
2022-12-06 17:55:24 -0600

    aws db

--- src/test/java/com/company/FinalProject/controllers/GameControllerTest.java
+++ src/test/java/com/company/FinalProject/controllers/GameControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -31,8 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(GameController.class)
 public class GameControllerTest
 {
     @Autowired
